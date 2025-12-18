87335e2297f7821221725b5933802c59a7d08b68
Sultana Nawrin <nawrin.sultana@amd.com>
2025-12-18 10:58:45 +0000

    Add missing file

---
+++ comms/ctran/algos/AllToAll/AllToAllvImpl.cu
@@ -0,0 +1,21 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#include "comms/ctran/algos/AllToAll/AllToAllv.cuh"
+
+// Explicit template instantiations for AllToAllv kernels
+DECL_CTRAN_ALLTOALLV_KERN(int8_t);
+DECL_CTRAN_ALLTOALLV_KERN(uint8_t);
+DECL_CTRAN_ALLTOALLV_KERN(int32_t);
+DECL_CTRAN_ALLTOALLV_KERN(uint32_t);
+DECL_CTRAN_ALLTOALLV_KERN(int64_t);
+DECL_CTRAN_ALLTOALLV_KERN(uint64_t);
+DECL_CTRAN_ALLTOALLV_KERN(half);
+DECL_CTRAN_ALLTOALLV_KERN(float);
+DECL_CTRAN_ALLTOALLV_KERN(double);
+#if defined(__CUDA_BF16_TYPES_EXIST__) || defined(__HIP_PLATFORM_AMD__)
+DECL_CTRAN_ALLTOALLV_KERN(__nv_bfloat16);
+#endif
+#if defined(__CUDA_FP8_TYPES_EXIST__) && defined(NCCL_ENABLE_FP8)
+DECL_CTRAN_ALLTOALLV_KERN(__nv_fp8_e4m3);
+DECL_CTRAN_ALLTOALLV_KERN(__nv_fp8_e5m2);
+#endif
