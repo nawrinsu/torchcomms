254dcee28ce4f170882e8bd415dc8c64526b27cf
Sultana Nawrin <nawrin.sultana@amd.com>
2025-12-19 00:48:18 +0000

    Set max nthreads to 512 for gfx950

--- comms/rcclx/develop/src/include/rccl_common.h
+++ comms/rcclx/develop/src/include/rccl_common.h
@@ -45,10 +45,10 @@ typedef enum RcclTunableColls {
 #define RCCL_PROTOCOL_FACTOR_IDX 2
 #define RCCL_PROTOCOL_THREAD_THRESHOLD_IDX 3
 
-#define RCCL_SINGLE_NODE_MAX_NTHREADS 256
+#define RCCL_SINGLE_NODE_MAX_NTHREADS 512
 // 512 is causing invalid ISA errors, so use 256 instead
 // #define RCCL_GFX950_MAX_NTHREADS 512  // for Simple and LL64/LL128 gfx950
-#define RCCL_GFX950_MAX_NTHREADS 256  // for Simple and LL64/LL128 gfx950
+#define RCCL_GFX950_MAX_NTHREADS 512  // for Simple and LL64/LL128 gfx950
 #define RCCL_DEFAULT_MAX_NTHREADS 256 // for Simple and LL64/LL128 other archs
 #define RCCL_LL_MAX_NTHREADS 256
 #define RCCL_P2P_MAX_NTHREADS 256
